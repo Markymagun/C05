4c7cebb2870dd72de7c5a7a40c1e9bdb74f456ac
Jiraphat Parteep <jparteep@z2t14c1.42bangkok.com>
2026-05-18 20:45:35 +0700

    4

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -7,9 +7,33 @@ int is_safe(int *board, int current_col, int test_row)
     i = 0;
     while (i < current_col)
     {
-        
+        if (board[i] == test_row || current_col - i == test_row - board[i] || 
+        current_col - i == board[i] - test_row)
+        {
+            return (0);
+        }
+        i++;
     }
+    return (1);
 }
+
+int solve_queens(int *board, int col)
+{
+    int i = 0;
+    int count;
+
+    if (col == 10)
+    {
+        while (i != 10)
+        {
+            write(1, &board[i], 1);
+            write(1, "\n", 1);
+        }
+    }
+
+    while (i)
+}
+
 int ft_ten_queens_puzzle(void)
 {
     int board[10];
